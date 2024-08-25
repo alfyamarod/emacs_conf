b6640b984bd148a93cad86d5199c91f8826be09f
alf <alfyamamotorod@gmail.com>
2024-08-25 21:18:10 +0200

    emacs conf

---
+++ init.el
@@ -0,0 +1,407 @@
+;; Dont show init screen
+(setq inhibit-startup-message t)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(load-theme 'tango-dark)
+
+;;font
+(set-frame-font "JetBrainsMono Nerd Font 12" nil t)
+
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+
+(setq c-default-style "linux"
+      c-basic-offset 4)
+
+;; Package sources
+(require 'package)
+(setq package-archives '(("melpa". "https://melpa.org/packages/")
+			 ("org". "https://orgmode.org/elpa/")
+			 ("elpa". "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents (package-refresh-contents))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Completion
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+	 :map ivy-minibuffer-map
+	 ("TAB" . ivy-alt-done)
+	 ("C-l" . ivy-alt-done)
+	 ("C-j" . ivy-next-line)
+	 ("C-k" . ivy-previous-line)
+	 :map ivy-switch-buffer-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-l" . ivy-done)
+	 ("C-d" . ivy-switch-buffer-kill)
+	 :map ivy-reverse-i-search-map
+	 ("C-k" . ivy-previous-line)
+	 ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package all-the-icons)
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package general
+  :ensure t
+  :config
+  (general-evil-setup t)
+  (general-create-definer alf/leader-keys
+   :states '(normal insert visual emacs motion)
+   :keymaps 'override
+   :prefix "SPC"
+   :global-prefix "C-SPC")
+  (alf/leader-keys
+    "w"  '(:ignore t :which-key "window")
+    "w h" '(windmove-left :which-key "move left")
+    "w j" '(windmove-down :which-key "move down")
+    "w k" '(windmove-up :whic-key "move up")
+    "w l" '(windmove-right :which-key "move right")
+    ))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :custom ((projectile-completion-system 'ivy))
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  ;; NOTE: Set this to the folder where you keep your Git repos!
+  (when (file-directory-p "~/work")
+    (setq projectile-project-search-path '("~/work")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+
+;; hydra for temporary commands
+(use-package hydra)
+
+
+;; languages and completion
+(use-package lsp-mode
+  :ensure t
+  :defer t
+  :hook ((c-mode . lsp)
+	 (lsp-mode . lsp-enable-which-key-integration))
+  :commands (lsp lsp-deffered)
+  :init
+  (setq lsp-keymap "C-c l")
+  (setq lsp-file-watch-threshold 15000)
+
+  (setq lsp-clients-clangd-args '("--header-insertion-decorators=0"
+                                  "--clang-tidy"
+                                  "--enable-config"))
+  
+  (setopt lsp-log-io nil)
+  (setopt lsp-log-max 0)
+  (setopt lsp-headerline-breadcrumb-enable nil)
+  )
+
+
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024)
+      treemacs-space-between-root-nodes nil
+      company-idle-delay 0.0
+      company-minimum-prefix-length 1
+      lsp-idle-delay 0.1)  ;; clangd is fast
+
+
+(use-package lsp-ui
+  :ensure t
+  :commands (lsp-ui-mode)
+  :config
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-doc-delay 0.5)
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  )
+
+(use-package lsp-treemacs
+  :defer t
+  :after lsp
+  :config
+  (setq lsp-treemacs-sync-mode 1)
+  )
+
+(use-package lsp-ivy
+  :defer t
+  :after lsp
+  :commands lsp-ivy-workspace-symbol)
+
+(use-package corfu
+  :ensure t
+  ;; Optional customizations
+  :custom
+  (corfu-cycle t)                 ; Allows cycling through candidates
+  (corfu-auto t)                  ; Enable auto completion
+  (corfu-auto-prefix 2)
+  (corfu-auto-delay 0.8)
+  (corfu-popupinfo-delay '(0.5 . 0.2))
+  (corfu-preview-current 'insert) ; insert previewed candidate
+  (corfu-preselect 'prompt)
+  (corfu-on-exact-match nil)      ; Don't auto expand tempel snippets
+  ;; Optionally use TAB for cycling, default is `corfu-complete'.
+  :bind (:map corfu-map
+              ("M-SPC"      . corfu-insert-separator)
+              ("TAB"        . corfu-next)
+              ([tab]        . corfu-next)
+              ("S-TAB"      . corfu-previous)
+              ([backtab]    . corfu-previous)
+              ("S-<return>" . corfu-insert)
+              ("RET"        . nil))
+
+  :init
+  (global-corfu-mode)
+  (corfu-history-mode)
+  (corfu-popupinfo-mode) ; Popup completion info
+  :config
+  (add-hook 'eshell-mode-hook
+            (lambda () (setq-local corfu-quit-at-boundary t
+                                   corfu-quit-no-match t
+                                   corfu-auto nil)
+              (corfu-mode))
+            nil
+            t))
+
+ (setq treesit-language-source-alist
+        '((bash "https://github.com/tree-sitter/tree-sitter-bash")
+          (c "https://github.com/tree-sitter/tree-sitter-c")
+          (cmake "https://github.com/uyha/tree-sitter-cmake")
+          (common-lisp "https://github.com/theHamsta/tree-sitter-commonlisp")
+          (cpp "https://github.com/tree-sitter/tree-sitter-cpp")
+          (elisp "https://github.com/Wilfred/tree-sitter-elisp")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (make "https://github.com/alemuller/tree-sitter-make")
+          (markdown "https://github.com/ikatyang/tree-sitter-markdown")
+          (python "https://github.com/tree-sitter/tree-sitter-python")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+
+
+
+
+;; LATEX
+
+(use-package auctex
+  :defer t 
+:hook (tex-mode . lsp-deferred)
+  :hook (latex-mode . lsp-deferred)
+  :init
+  (setq TeX-command-default   (if (executable-find "latexmk") "LatexMk" "LaTeX")
+        TeX-engine            (if (executable-find "xetex")   'xetex    'default)
+        TeX-auto-save                     t
+        TeX-parse-self                    t
+        TeX-syntactic-comment             t
+        TeX-auto-local                    ".auctex-auto"
+        TeX-style-local                   ".auctex-style"
+        TeX-source-correlate-mode         t
+        TeX-source-correlate-method       'synctex
+        TeX-source-correlate-start-server nil
+        TeX-electric-sub-and-superscript  t
+        TeX-fill-break-at-separators      nil
+        TeX-save-query                    t)
+  :config
+  (setq font-latex-match-reference-keywords
+        '(;; BibLaTeX.
+          ("printbibliography" "[{") ("addbibresource" "[{")
+          ;; Standard commands.
+          ("cite" "[{")       ("citep" "[{")
+          ("citet" "[{")      ("Cite" "[{")
+          ("parencite" "[{")  ("Parencite" "[{")
+          ("footcite" "[{")   ("footcitetext" "[{")
+          ;; Style-specific commands.
+          ("textcite" "[{")   ("Textcite" "[{")
+          ("smartcite" "[{")  ("Smartcite" "[{")
+          ("cite*" "[{")      ("parencite*" "[{")
+          ("supercite" "[{")
+          ;; Qualified citation lists.
+          ("cites" "[{")      ("Cites" "[{")
+          ("parencites" "[{") ("Parencites" "[{")
+          ("footcites" "[{")  ("footcitetexts" "[{")
+          ("smartcites" "[{") ("Smartcites" "[{")
+          ("textcites" "[{")  ("Textcites" "[{")
+          ("supercites" "[{")
+          ;; Style-independent commands.
+          ("autocite" "[{")   ("Autocite" "[{")
+          ("autocite*" "[{")  ("Autocite*" "[{")
+          ("autocites" "[{")  ("Autocites" "[{")
+          ;; Text commands.
+          ("citeauthor" "[{") ("Citeauthor" "[{")
+          ("citetitle" "[{")  ("citetitle*" "[{")
+          ("citeyear" "[{")   ("citedate" "[{")
+          ("citeurl" "[{")
+          ;; Special commands.
+          ("fullcite" "[{")
+          ;; Cleveref.
+          ("cref" "{")          ("Cref" "{")
+          ("cpageref" "{")      ("Cpageref" "{")
+          ("cpagerefrange" "{") ("Cpagerefrange" "{")
+          ("crefrange" "{")     ("Crefrange" "{")
+          ("labelcref" "{")))
+
+  (setq font-latex-match-textual-keywords
+        '(;; BibLaTeX brackets.
+          ("parentext" "{") ("brackettext" "{")
+          ("hybridblockquote" "[{")
+          ;; Auxiliary commands.
+          ("textelp" "{")   ("textelp*" "{")
+          ("textins" "{")   ("textins*" "{")
+          ;; Subcaption.
+          ("subcaption" "[{")))
+
+  (setq font-latex-match-variable-keywords
+        '(;; Amsmath.
+          ("numberwithin" "{")
+          ;; Enumitem.
+          ("setlist" "[{")     ("setlist*" "[{")
+          ("newlist" "{")      ("renewlist" "{")
+          ("setlistdepth" "{") ("restartlist" "{")
+          ("crefname" "{")))
+  (setq TeX-master t)
+  (setcar (cdr (assoc "Check" TeX-command-list)) "chktex -v6 -H %s")
+  (add-hook 'TeX-mode-hook (lambda ()
+                             (setq ispell-parser          'tex
+                                   fill-nobreak-predicate (cons #'texmathp fill-nobreak-predicate))))
+  (add-hook 'TeX-mode-hook #'visual-line-mode)
+  (add-hook 'TeX-update-style-hook #'rainbow-delimiters-mode)
+  :general
+  (phundrak/major-leader-key
+    :packages 'lsp-mode
+    :keymaps  '(latex-mode-map LaTeX-mode-map)
+   "l"  '(:keymap lsp-command-map :which-key "lsp"))
+  (phundrak/major-leader-key
+    :packages 'auctex
+    :keymaps  '(latex-mode-map LaTeX-mode-map)
+    "v" '(TeX-view            :which-key "View")
+    "c" '(TeX-command-run-all :which-key "Compile")
+    "m" '(TeX-command-master  :which-key "Run a command")))
+
+
+(use-package tex-mode
+  :defer t
+  :config
+  (setq LaTeX-section-hook '(LaTeX-section-heading
+                             LaTeX-section-title
+                             LaTeX-section-toc
+                             LaTeX-section-section
+                             LaTeX-section-label)
+        LaTeX-fill-break-at-separators nil
+        LaTeX-item-indent              0))
+
+(use-package adaptive-wrap
+  :defer t
+  :after auctex
+  :hook (LaTeX-mode . adaptative-wrap-prefix-mode)
+  :init (setq-default adaptative-wrap-extra-indent 0))
+
+(use-package auctex-latexmk
+  :after auctex
+  :defer t
+  :init
+  (setq auctex-latexmk-inherit-TeX-PDF-mode t)
+  (add-hook 'LaTeX-mode (lambda () (setq TeX-command-default "LatexMk")))
+  :config
+  (auctex-latexmk-setup))
+
+(use-package company-auctex
+  :defer t
+  :after (company auctex)
+  :config
+  (company-auctex-init))
+
+(use-package company-math
+  :defer t
+  :after (company auctex)
+  :config
+  (defun my-latex-mode-setup ()
+    (setq-local company-backends
+                (append '((company-math-symbols-latex company-latex-commands))
+                        company-backends)))
+  (add-hook 'TeX-mode-hook #'my-latex-mode-setup))
+
+
+(use-package evil-nerd-commenter
+  :after evil)
+
+(use-package treemacs
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "SCP tt") #'treemacs-select-window))
+  (setq treemacs-follow-after-init t
+        treemacs-is-never-other-window t
+        treemacs-sorting 'alphabetic-case-insensitive-asc)
+
+  :config
+  (treemacs-follow-mode -1))
+
+;(use-package treemacs-nerd-icons
+;  :defer t
+;  :config (treemacs-load-theme "nerd-icons"))
+
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
